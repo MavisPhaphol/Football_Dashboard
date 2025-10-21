22cda0524193a75cc60e3a52c7e1a18e93f91e64
Mavis Phaphol <114840330+MavisPhaphol@users.noreply.github.com>
2025-10-21 21:58:27 +0000

    Implement initial Flask application and create homepage template

--- application.py
+++ application.py
@@ -1,1 +1,8 @@
-#Flask
+from flask import Flask, render_template
+
+application = Flask(__name__)
+@application.route('/')
+def dashboard():
+    return render_template('index.html')
+if __name__ == '__main__':
+    application.run(debug=True)
