b952da02992af7c482770b1ac705d9093ecc571e
Mavis Phaphol <114840330+MavisPhaphol@users.noreply.github.com>
2025-11-07 02:09:12 +0000

    Add routes and templates for college football teams with navigation

--- application.py
+++ application.py
@@ -4,5 +4,26 @@ application = Flask(__name__)
 @application.route('/')
 def dashboard():
     return render_template('index.html')
+
+@application.route('/ohioState')
+def ohioState():
+    return render_template('ohioState.html')
+
+@application.route('/houston')
+def houston():
+    return render_template('houston.html')
+
+@application.route('/georgia')
+def georgia():
+    return render_template('georgia.html')
+
+@application.route('/lsu')
+def lsu():
+    return render_template('lsu.html')
+
+@application.route('/tennessee')
+def tennessee():
+    return render_template('tennessee.html')
+
 if __name__ == '__main__':
     application.run(debug=True)
