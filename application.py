4e9060f65fbedb1b468dbbcd9dd0e112aa5d8c2c
Mavis Phaphol <114840330+MavisPhaphol@users.noreply.github.com>
2026-01-05 03:46:06 +0000

    Updated the nav bar and added CSS for the nav bar and footer

--- application.py
+++ application.py
@@ -7,22 +7,27 @@ def dashboard():
 
 @application.route('/ohioState')
 def ohioState():
+    teamID=1
     return render_template('ohioState.html')
 
 @application.route('/houston')
 def houston():
+    teamID=2
     return render_template('houston.html')
 
 @application.route('/georgia')
 def georgia():
+    teamID=3
     return render_template('georgia.html')
 
 @application.route('/lsu')
 def lsu():
+    teamID=4
     return render_template('lsu.html')
 
 @application.route('/tennessee')
 def tennessee():
+    teamID=5
     return render_template('tennessee.html')
 
 if __name__ == '__main__':
